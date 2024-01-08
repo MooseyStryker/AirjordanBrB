88c679636741ded1db08c574ab87f8cf8943a37e
Stryker Huber <stryker.important@gmail.com>
2024-01-08 07:58:20 -0700

    Final Draft 3

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -536,23 +536,26 @@ router.post('/', restoreUser, requireAuth, async (req, res, next) => {
         })
         res.status(201).json(newGroup)
     } catch (error) {
-        if(error instanceof Sequelize.ValidationError) {
+      
+        if (error instanceof Sequelize.ValidationError) {
+            let errors = {};
+            error.errors.forEach(e => {
+              errors[e.path] = e.message;
+            });
 
             return res.status(400).json({
-                message: 'Validation Error',
-                errors: error.errors.map(e => {
-                  (e);
-                  return {
-                    [e.path]: e.message
-                  };
-                })
-              });
-        }
+              message: 'Validation Error',
+              errors: errors
+            });
+          }
 
-        res.status(400).json({
+          res.status(400).json({
             message: 'Bad Request'
-        })
+          });
+
     }
+
+
 })
 
 
