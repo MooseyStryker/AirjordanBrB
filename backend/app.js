d4df0c5783911dacbe8307b7f8e6217e609dcecd
Stryker Huber <stryker.important@gmail.com>
2024-01-23 14:41:11 -0700

    one chang2

--- backend/app.js
+++ backend/app.js
@@ -76,12 +76,19 @@ app.use((err, _req, _res, next) => {
 app.use((err, _req, res, _next) => {
   res.status(err.status || 500);
   console.error(err);
-  res.json({
-    title: isProduction ? '' : err.title || 'Server Error',
-    message: err.message,
-    errors: err.errors,
-    stack: isProduction ? null : err.stack
-  });
+
+  if (isProduction) {
+    return res.json ({
+      message: err.errors.message
+    })
+  } else {
+    return res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+  }
 });
 
 module.exports = app;
