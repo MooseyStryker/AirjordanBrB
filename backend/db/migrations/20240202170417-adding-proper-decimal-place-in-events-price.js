7c4e781a67cd7ac96387fbd16fc4a52d871f68a9
Stryker Huber <stryker.important@gmail.com>
2024-02-02 10:49:51 -0700

    Fixed events migration options key

--- backend/db/migrations/20240202170417-adding-proper-decimal-place-in-events-price.js
+++ backend/db/migrations/20240202170417-adding-proper-decimal-place-in-events-price.js
@@ -7,21 +7,23 @@ if (process.env.NODE_ENV === 'production'){
   options.schema = process.env.SCHEMA // Defines your schema in options object
 }
 
+options.tableName = "Events"
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.changeColumn('Events', 'price', {
+    await queryInterface.changeColumn(options, 'price', {
       type: Sequelize.DECIMAL(10, 2),
       allowNull: false
-    }, options);
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.changeColumn('Events', 'price', {
+    await queryInterface.changeColumn(options, 'price', {
 
       type: Sequelize.DECIMAL,
       allowNull: false
 
-    }, options);
+    });
   }
 };
